5f0240f26fda77a0a5bf89d4db9564d56d40d204
ac <ac@ap-o.com>
2017-08-07 11:24:20 +0100

    use config variable for mongoose connect

--- src/server/db/mongoose_connect.js
+++ src/server/db/mongoose_connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-const config = require('../config');
+const { mongo_uri } = require('../config');
 
-const mongooseConnection = mongoose.connect(config.mongo_uri, {
+const mongooseConnection = mongoose.connect(mongo_uri, {
   useMongoClient : true,
   keepAlive      : 1
 });
