b8c8445c6965e8a48476e9730a6e713607be92de
ac <ac@ap-o.com>
2017-08-21 10:17:09 +0100

    adds emotion > erratic behaviour, won’t run on refresh

--- src/browser/components/layout_login/index.js
+++ src/browser/components/layout_login/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'emotion/react';
+
+const Container = styled('section')`
+  display: flex;
+  flex-direction: column;
+  background-color: #e0e0e0;
+  min-height: 100vh;
+  flex: 1 0 100%;
+`;
+
+const HeaderOne = styled('h1')`
+  font-size: 3.4rem;
+  line-height: 1em;
+  margin: 0 0 0.67em 0;
+`;
 
 const Login = () => {
   return (
-    <section>
-      <h1>Login</h1>
+    <Container>
+      <HeaderOne>Login</HeaderOne>
       <a href="http://localhost:3000/auth/google">Login with google</a>
-    </section>
+    </Container>
   );
 };
 
